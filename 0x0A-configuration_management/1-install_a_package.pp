3ff11e54c3a492da50d45b2bac01f6639200c4b5
hajoura89 <hajarkammou@gmail.com>
2023-10-27 16:11:50 -0700

    Werkzeug install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #!/usr/bin/pup
 # Install version 2.1.0 of  flask
-package {'flask':
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => pip3,
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => pip3,
 }
